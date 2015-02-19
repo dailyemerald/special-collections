049649912db92607e73d9eaa2bde63d45aa930b1
Sierra Morgan <sierramorgan07@gmail.com>
2015-02-19 11:22:08 -0800

    little changes here and there

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-console.log(ctx);
-var img = document.getElementById("library");
-ctx.drawImage(img,10,10);
 
-document.onmousemove = function(e) {
-  console.log(e)
-  if (event.pageY < 200 & event.pageX < 200) {
-    queue();
-  }
-  else{
-  	//do nothing 
-  }
-}
